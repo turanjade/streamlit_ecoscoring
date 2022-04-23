5729d9d5a0d437be2e853badd9b4af03aecafef6
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-24 00:30:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,8 @@ def run_query(query):
     return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
-#st.markdown(sheet_url)
-rows = run_query(f'A2:G9, SELECT * FROM"{sheet_url}"')
+st.markdown(sheet_url)
+rows = run_query(f'SELECT * FROME"{sheet_url}"')
 
 # Print results.
 st.write(rows)
