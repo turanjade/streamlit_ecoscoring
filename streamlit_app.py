0bcafbad6ee84dca235a03d365774a3ed908efd1
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-24 11:09:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,31 @@ from google.oauth2 import service_account
 #    ("Email", "Home phone", "Mobile phone")
 #)
 
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+st.write('connection created')
+
+# Perform SQL query on the Google Sheet. 
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+st.markdown(sheet_url)
+rows = run_query(f'SELECT * FROM"{sheet_url}"')
+
+# Print results.
+#st.write(rows)
+
 st.sidebar.subheader('Upload your GPS trajectory data')
 uploaded_file = st.sidebar.file_uploader(" ")
 if uploaded_file is not None:
@@ -51,26 +76,3 @@ st.map(map_data)
 
 
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
-
-# Perform SQL query on the Google Sheet. 
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["private_gsheets_url"]
-st.markdown(sheet_url)
-rows = run_query(f'SELECT * FROM"{sheet_url}"')
-
-# Print results.
-#st.write(rows)
