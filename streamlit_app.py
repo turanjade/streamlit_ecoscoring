3852640173f83162bef661fcb0c7c6bf1f2de5bf
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-24 13:07:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ from google.oauth2 import service_account
 
 st.title('This is Eco-score!')
 
-st.sidebar.button('Back to home')
-st.write("  ")
+if st.sidebar.button('Back to home'):
+    st.write("  ")
+
 
 #if st.sidebar.button('Loading trajectory'):
 gs_id = "1JznNtYSlTlOwmFq8baTR4Ws0r7f865wyPe2NG4m45a0"
@@ -24,16 +25,16 @@ st.write(df.head())
 #select a specific vehicle
 st.subheader('Choose one vehicle to visualize')
 vehoption = st.selectbox(' ', df.vehid.unique())
-st.write(df.vehid.unique())
 vehselected = df[df.vehid == vehoption]
 
 #select a spcific trip of that vehicle
-st.write('This vehicle has in total ', str(len(vehselected.orderid.unique())), ' trips')
+st.markdown('This vehicle has in total ', str(len(vehselected.orderid.unique())), ' trips')
 
 st.subheader('Choose one trip to visualize')
 tripoption = st.selectbox(' ', vehselected.orderid.unique())
 tripselected = vehselected[vehselected.orderid == tripoption]
 
+st.subheader('This is the trajectory')
 map_data = pd.DataFrame(columns = ['lat', 'lon'])
 map_data['lat'] = tripselected.latitude
 map_data['lon'] = tripselected.longitude
