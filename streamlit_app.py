8bf00d6d52521eb6a74eb72ed66b3f343613f92a
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-24 12:02:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,7 @@ sheet_url = st.secrets["private_gsheets_url"]
 st.markdown(sheet_url)
 
 result = conn.execute("""
-    SELECT
-        *
-    FROM
-        "https://docs.google.com/spreadsheets/d/1JznNtYSlTlOwmFq8baTR4Ws0r7f865wyPe2NG4m45a0/edit#gid=0"
+    SELECT orderid FROM "https://docs.google.com/spreadsheets/d/1JznNtYSlTlOwmFq8baTR4Ws0r7f865wyPe2NG4m45a0/edit#gid=0" WHERE vehid = '9j22fi9gb62rB4luAgce49ahegf_qBbg'
 """, headers=1)
 for rows in result:
     st.write(rows)
