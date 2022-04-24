a0726abf8fe869a860038e2e36eb54fe50ced135
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-24 12:54:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 
 st.title('This is Eco-score!')
 
-
+st.sidebar.button('Back to home')
 
 if st.sidebar.button('Loading trajectory'):
     gs_id = "1JznNtYSlTlOwmFq8baTR4Ws0r7f865wyPe2NG4m45a0"
@@ -18,7 +18,7 @@ if st.sidebar.button('Loading trajectory'):
     df = pd.read_csv(gs_url)
     st.write(df.head())
 
-if st.sidebar.button('Upload your GPS trajectory data'):
+else if st.sidebar.button('Upload your GPS trajectory data'):
     uploaded_file = st.sidebar.file_uploader(" ")
     if uploaded_file is not None:
         # Can be used wherever a "file-like" object is accepted:
